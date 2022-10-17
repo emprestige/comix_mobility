1884f237b35a9a0a0a2e6c3d4931cc8ac730a6aa
emiel200 <69582748+emiel200@users.noreply.github.com>
2022-10-17 13:40:32 +0100

    Add files via upload

---
+++ an_sandbox.R
@@ -0,0 +1,37 @@
+##sandbox
+
+library(ggplot2)
+ 
+data_path <-"C:\\Users\\emiel\\OneDrive\\Documents\\LSHTM\\Fellowship\\Project\\Data\\"
+
+#import contact data
+cnts <- qs::qread(file.path(data_path,"part_cnts.qs"))
+                  
+#import mobility data
+mob <- qs::qread(file.path(data_path, "google_mob.qs"))
+
+#visualisation 
+
+#retail and recreation
+ggplot(data = mob, aes(date, retail_and_recreation_percent_change_from_baseline)) + 
+ geom_line(group = 1) 
+
+#grocery and pharmacy
+ggplot(data = mob, aes(date, grocery_and_pharmacy_percent_change_from_baseline)) + 
+  geom_line(group = 1)
+
+#parks
+ggplot(data = mob, aes(date, parks_percent_change_from_baseline)) + 
+  geom_line(group = 1)
+
+#transit stations
+ggplot(data = mob, aes(date, transit_stations_percent_change_from_baseline)) + 
+  geom_line(group = 1)
+
+#workplaces
+ggplot(data = mob, aes(date, workplaces_percent_change_from_baseline)) + 
+  geom_line(group = 1)
+
+#residential
+ggplot(data = mob, aes(date, residential_percent_change_from_baseline)) + 
+  geom_line(group = 1)
---
+++ dm_format_google.R
@@ -0,0 +1,17 @@
+##data management for mobility data
+
+library(data.table)
+
+data_path <-"C:\\Users\\emiel\\OneDrive\\Documents\\LSHTM\\Fellowship\\Project\\Data\\"
+
+mob_20 <- as.data.table(read.csv(file.path(data_path, "2020_GB_Region_Mobility_Report.csv")))
+mob_21 <- as.data.table(read.csv(file.path(data_path, "2021_GB_Region_Mobility_Report.csv")))
+mob_22 <- as.data.table(read.csv(file.path(data_path, "2022_GB_Region_Mobility_Report.csv")))
+
+UK_20 <- mob_20[sub_region_1 == ""]
+UK_21 <- mob_21[sub_region_1 == ""]
+UK_22 <- mob_22[sub_region_1 == ""]
+
+UK_mob <- rbind(UK_20, UK_21, UK_22)
+
+qs::qsave(UK_mob, "C:\\Users\\emiel\\OneDrive\\Documents\\LSHTM\\Fellowship\\Project\\Data\\google_mob.qs")
