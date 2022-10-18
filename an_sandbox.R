fd9ce9fcc8088d337ad0d297805cb0f8ab516195
Em Prestige <emiel200@gmail.com>
2022-10-18 15:51:00 +0100

    first attempt at cowplots to compare goggle mobility data with comix data

--- an_sandbox.R
+++ an_sandbox.R
@@ -3,8 +3,9 @@
 #load libraries
 library(ggplot2)
 library(tidyverse)
- 
-#set daa path
+library(cowplot) 
+
+#set data path
 data_path <-"C:\\Users\\emiel\\Documents\\LSHTM\\Fellowship\\Project\\comix_mobility\\Data\\"
 
 #import contact data
@@ -41,5 +42,33 @@ ggplot(data = mob, aes(date, residential_percent_change_from_baseline)) +
 
 ##calculate mean contacts for each survey round 
 
-#survey rounds
-#ans <- cnts[substr(part_wave_uid, 3, 4) == , m_all := mean(n_cnt)]
+#mean contacts for all, home, and work -- easily comparable to google mobility
+all <-  cnts[, mean(n_cnt),      by = substr(part_wave_uid, 4, 6)]
+home <- cnts[, mean(n_cnt_home), by = substr(part_wave_uid, 4, 6)]
+work <- cnts[, mean(n_cnt_work), by = substr(part_wave_uid, 4, 6)]
+
+##calculate mean baseline change for each week
+mob_sub <- mob[date >= "2020-03-24" & date <= "2022-03-02"]
+g_home <- mob_sub[, mean(residential_percent_change_from_baseline), 
+                  by = list(yw = paste(year(date), week(date)))]
+g_work <- mob_sub[, mean(workplaces_percent_change_from_baseline),
+                  by = list(yw = paste(year(date), week(date)))]
+
+##visualisation - contact data 
+
+#workplaces 
+ggplot(data = work, aes(substr, V1)) + geom_line(group = 1)
+
+#homes
+ggplot(data = home, aes(substr, V1)) + geom_line(group = 1)
+
+##visualisation - combination
+w1 <- ggplot(data = g_work, aes(yw, V1)) + geom_line(group = 1)
+w2 <- ggplot(data = work, aes(substr, V1)) + geom_line(group = 1)
+
+plot_grid(w1, w2)
+
+h1 <- ggplot(data = g_home, aes(yw, V1)) + geom_line(group = 1)
+h2 <- ggplot(data = home, aes(substr, V1)) + geom_line(group = 1)
+
+plot_grid(h1, h2)
