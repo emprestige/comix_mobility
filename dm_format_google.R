99b59c9a8b7c747910123e37ebcf1ed955c51198
Em Prestige <emiel200@gmail.com>
2022-10-18 13:05:28 +0100

    change from read.cvs to fread

--- dm_format_google.R
+++ dm_format_google.R
@@ -4,9 +4,9 @@ library(data.table)
 
 data_path <-"C:\\Users\\emiel\\Documents\\LSHTM\\Fellowship\\Project\\comix_mobility\\Data\\"
 
-mob_20 <- as.data.table(read.csv(file.path(data_path, "2020_GB_Region_Mobility_Report.csv")))
-mob_21 <- as.data.table(read.csv(file.path(data_path, "2021_GB_Region_Mobility_Report.csv")))
-mob_22 <- as.data.table(read.csv(file.path(data_path, "2022_GB_Region_Mobility_Report.csv")))
+mob_20 <- fread(file.path(data_path, "2020_GB_Region_Mobility_Report.csv"))
+mob_21 <- fread(file.path(data_path, "2021_GB_Region_Mobility_Report.csv"))
+mob_22 <- fread(file.path(data_path, "2022_GB_Region_Mobility_Report.csv"))
 
 UK_20 <- mob_20[sub_region_1 == ""]
 UK_21 <- mob_21[sub_region_1 == ""]
