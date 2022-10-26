a9b21621c313876630db662d7ec37daf9af11960
Em Prestige <emiel200@gmail.com>
2022-10-26 16:16:38 +0100

    first attempt at contact matrices

--- an_GAM_prac.R
+++ an_GAM_prac.R
@@ -1,25 +1,27 @@
 ##GAM practice
 
 #load libraries
+library(data.table)
 library(ggplot2)
 library(tidyverse)
 library(cowplot) 
 library(ggpubr)
 library(mgcv)
+library(socialmixr)
 
 #set data path
 data_path <-"C:\\Users\\emiel\\Documents\\LSHTM\\Fellowship\\Project\\comix_mobility\\Data\\"
-
-#import contact data
-cnts <- qs::qread(file.path(data_path,"part_cnts.qs"))
-
-#order by date
-#cnts_date <- cnts[order(date)]
-cnts_date <- cnts %>%
-  arrange(date)
 
 #import mobility data
 mob <- qs::qread(file.path(data_path, "google_mob.qs"))
 
 #subset for same date range
 mob_sub <- mob[date >= "2020-03-24" & date <= "2022-03-02"]
+
+##
+dir_data_validate <- "C:\\Users\\emiel\\Filr\\Net Folders\\EPH Shared\\Comix_survey\\data\\validated\\"
+pt <- qs::qread(file.path(dir_data_validate, "part_min.qs"))
+ct <- qs::qread(file.path(dir_data_validate, "contacts.qs"))
+
+new_survey <- survey(pt, ct)
+contact_matrix(new_survey)
