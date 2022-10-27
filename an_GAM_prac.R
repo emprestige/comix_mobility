bf0dbab5500035ca2272d923e482d9b214ecfd76
Em Prestige <emiel200@gmail.com>
2022-10-27 12:11:34 +0100

    minor changes but still no progress on GAMs

--- an_GAM_prac.R
+++ an_GAM_prac.R
@@ -19,9 +19,22 @@ mob <- qs::qread(file.path(data_path, "google_mob.qs"))
 mob_sub <- mob[date >= "2020-03-24" & date <= "2022-03-02"]
 
 ##
+#set data path 
 dir_data_validate <- "C:\\Users\\emiel\\Filr\\Net Folders\\EPH Shared\\Comix_survey\\data\\validated\\"
+
+#import participant and contact data 
 pt <- qs::qread(file.path(dir_data_validate, "part_min.qs"))
 ct <- qs::qread(file.path(dir_data_validate, "contacts.qs"))
 
-new_survey <- survey(pt, ct)
-contact_matrix(new_survey)
+#filter to just uk 
+pt_uk <- pt[country == "uk"]
+ct_uk <- ct[country == "uk"]
+
+#filter down to one time point 
+pt_uk1 <- pt_uk[date == "2020-04-19"]
+ct_uk1 <- ct_uk[date == "2020-04-19"]
+#pt_uk1[is.na(pt_uk1)] <- 0
+#ct_uk1[is.na(ct_uk1)] <- 0
+
+#new_survey <- survey(pt_uk1, ct_uk1)
+#contact_matrix(new_survey)
