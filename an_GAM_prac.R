99dcec81ca166132238f01b26202e1f33e682b3f
Em Prestige <emiel200@gmail.com>
2022-10-27 15:44:14 +0100

    building contact matrix and plotting

--- an_GAM_prac.R
+++ an_GAM_prac.R
@@ -18,23 +18,17 @@ mob <- qs::qread(file.path(data_path, "google_mob.qs"))
 #subset for same date range
 mob_sub <- mob[date >= "2020-03-24" & date <= "2022-03-02"]
 
-##
-#set data path 
-dir_data_validate <- "C:\\Users\\emiel\\Filr\\Net Folders\\EPH Shared\\Comix_survey\\data\\validated\\"
+#import participant and contact data
+pt <- qs::qread(file.path(data_path, "participants.qs"))
+ct <- qs::qread(file.path(data_path, "contact.qs"))
 
-#import participant and contact data 
-pt <- qs::qread(file.path(dir_data_validate, "part_min.qs"))
-ct <- qs::qread(file.path(dir_data_validate, "contacts.qs"))
+#filter down to one survey round
+pt21 <- pt[survey_round == "21"]
+ct21 <- ct[survey_round == "21"]
 
-#filter to just uk 
-pt_uk <- pt[country == "uk"]
-ct_uk <- ct[country == "uk"]
+new_survey <- survey(pt21, ct21)
+round21 <- contact_matrix(new_survey, age.limits = c(0, 5, 12, 18, 30, 40, 50, 60, 65, 70))
 
-#filter down to one time point 
-pt_uk1 <- pt_uk[date == "2020-04-19"]
-ct_uk1 <- ct_uk[date == "2020-04-19"]
-#pt_uk1[is.na(pt_uk1)] <- 0
-#ct_uk1[is.na(ct_uk1)] <- 0
-
-#new_survey <- survey(pt_uk1, ct_uk1)
-#contact_matrix(new_survey)
+#plot contact matrix
+dt = melt(data.table(round21$matrix, keep.rownames = TRUE) , id.vars = c("rn"))
+ggplot(data = dt, aes(x = rn, y = variable, fill = value)) + geom_tile()
