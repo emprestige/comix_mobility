ada3c62719ff21976bcbb4532a5282430355c0d4
Em Prestige <emiel200@gmail.com>
2022-10-27 14:37:50 +0100

    small amounts of data cleaning

---
+++ dm_clean_comix.R
@@ -0,0 +1,35 @@
+##clean data for contact matrix 
+
+#load libraries
+library(data.table)
+library(tidyverse)
+library(socialmixr)
+
+#set data path 
+dir_data_validate <- "C:\\Users\\emiel\\Filr\\Net Folders\\EPH Shared\\Comix_survey\\data\\validated\\"
+
+#import participant and contact data 
+pt <- qs::qread(file.path(dir_data_validate, "part_min.qs"))
+ct <- qs::qread(file.path(dir_data_validate, "contacts.qs"))
+
+#first filter out unnecessary data (not from UK)
+pt_uk <- pt[country == "uk"]
+ct_uk <- ct[country == "uk"]
+
+#recode NA values 
+var_list <- names(ct_uk)
+for (j in var_list){
+  set(ct_uk,which(is.na(pt_cnt[[j]])),j,0)
+}
+
+ages <- ct_uk %>% group_by(cnt_age_group) %>% tally()
+
+#filter out unnecessary variables 
+ct_nmes <- c("part_id", "cnt_age_est_min", "cnt_age_est_max", 
+             "cnt_gender", "cnt_home", "cnt_work", "cnt_school",
+             "cnt_public_transport", "cnt_other", "survey_round")
+ct_uk <- ct_uk[, ..ct_nmes]
+
+#save data
+qs::qsave(pt_uk, "C:\\Users\\emiel\\Documents\\LSHTM\\Fellowship\\Project\\comix_mobility\\Data\\participants.qs")
+qs::qsave(ct_uk, "C:\\Users\\emiel\\Documents\\LSHTM\\Fellowship\\Project\\comix_mobility\\Data\\contact.qs")
