fd1b25569d08b3f345eb5a2fbe6ca8c9f4fe8512
Em Prestige <emiel200@gmail.com>
2022-10-31 15:27:07 +0000

    GAM for weight predictor for retail, transit, and grocery/pharmacy

--- an_GAM_prac.R
+++ an_GAM_prac.R
@@ -29,26 +29,19 @@ workplaces <- frac_sub[, .(mean_work_f = mean(1 + (workplaces_percent_change_fro
 mob_v_cnt <- merge(work, workplaces, by = "fortnight", all.x = F)
 colnames(mob_v_cnt) <- c("fortnight", "work", "workplaces")
   
-#test out models
+#model using GAM
 gam1 <- gam(work ~ s(workplaces), data = mob_v_cnt)
-summary(gam1)
-plot(gam1, pages = 1)
-pred <- predict(gam1)
-mob_v_cnt$pred <- pred
 
 #predict using 'new' data
 work_f = data.table(workplaces = seq(0, 1.25, by = 0.01));
 work_f[, work := pmax(0.0, predict(gam1, work_f, type = "response"))]
 
 #plot
-#ggplot(data = mob_v_cnt, aes(workplaces, work)) + geom_point() + 
-#  geom_smooth(aes(pred))
-
 plw = ggplot(mob_v_cnt) + 
-  geom_point(aes(x = workplaces, y = work), col = "red") + 
+  geom_point(aes(x = workplaces, y = work, col = "red")) + 
   geom_line(data = work_f, aes(x = workplaces, y = work)) +
   ylim(0, 3.5) + labs(x = "Google Mobility\n'workplaces' visits", 
-                      y = "Work contacts", colour = "Study") +
+                      y = "Work contacts") +
   theme(legend.position = "none")
 plw
 
@@ -56,32 +49,53 @@ plw
 ##can the same be done with home data??
 
 #get fornightly home data
-cnt_sub <- cnts_date[, fortnight := paste(year(date), "/", ceiling(week(date)/2))]
 home <- cnt_sub[, .(mean_cnt_home_f = mean(n_cnt_home)), by = fortnight]
 frac_sub <- mob_sub[, fortnight  := paste(year(date), "/", ceiling(week(date)/2))]
 residences <- frac_sub[, .(mean_home_f = mean(1 + (residential_percent_change_from_baseline)/100)), by = fortnight]
 mob_v_cnt2 <- merge(home, residences, by = "fortnight", all.x = F)
 colnames(mob_v_cnt2) <- c("fortnight", "home", "residences")
 
-#test out models
+#model using GAM
 gam2 <- gam(home ~ s(residences), data = mob_v_cnt2)
-summary(gam2)
-plot(gam2, pages = 1)
-pred <- predict(gam2)
-mob_v_cnt2$pred <- pred
 
 #predict using 'new' data
-home_f = data.table(residences = seq(0.8, 2, by = 0.01));
+home_f = data.table(residences = seq(0, 1.25, by = 0.01));
 home_f[, home := pmax(0.0, predict(gam2, home_f, type = "response"))]
 
 #plot
-#ggplot(data = mob_v_cnt2, aes(residences, home)) + geom_point() + 
-#  geom_smooth(aes(pred))
-
 plw2 = ggplot(mob_v_cnt2) + 
-  geom_point(aes(x = residences, y = home), col = "red") + 
+  geom_point(aes(x = residences, y = home, col = "red")) + 
   geom_line(data = home_f, aes(x = residences, y = home)) +
   ylim(0, 3.5) + labs(x = "Google Mobility\n'residential' visits", 
-                      y = "Home contacts", colour = "Study") +
+                      y = "Home contacts") +
   theme(legend.position = "none")
 plw2
+
+## weighted function for 'transit stations', 'retail and recreation', and 
+## 'grocery and pharmacy' visits
+
+#get fortnightly 'other' data
+other <- cnt_sub[, .(mean_cnt_other_f = mean(n_cnt_other)), by = fortnight]
+transit <- frac_sub[, .(mean_transit_f = mean(1 + (transit_stations_percent_change_from_baseline)/100)), by = fortnight]
+grocery_pharmacy <- frac_sub[, .(mean_gracery_f = mean(1 + (grocery_and_pharmacy_percent_change_from_baseline)/100)), by = fortnight]
+retail_recreation <- frac_sub[, .(mean_retail_f = mean(1 + (retail_and_recreation_percent_change_from_baseline)/100)), by = fortnight]
+mob_v_cnt3 <- merge(other, transit, by = "fortnight", all.x = F)
+mob_v_cnt3 <- merge(mob_v_cnt3, grocery_pharmacy, by = "fortnight", all.x = F)
+mob_v_cnt3 <- merge(mob_v_cnt3, retail_recreation, by = "fortnight", all.x = F)
+colnames(mob_v_cnt3) <- c("fortnight", "other","transit", "grocery_pharmacy", "retail_recreation")
+
+#create weighted predictor
+mob_v_cnt3[, predictor := retail_recreation * 0.345 + transit * 0.445 + grocery_pharmacy * 0.210] # See "optimisation" below for how this was arrived at
+
+#model using GAM
+model = gam(other ~ s(predictor), family = gaussian, data = mob_v_cnt3)
+other_f = data.table(predictor = seq(0, 1.25, by = 0.01));
+other_f[, other := pmax(0.0, predict(model, other_f, type = "response"))]
+
+plo = ggplot(mob_v_cnt3) + 
+  geom_point(aes(x = predictor, y = other, col = "red")) + 
+  geom_line(data = other_f, aes(x = predictor, y = other)) +
+  xlim(0, 1.25) + ylim(0, 5) + labs(x = "Google Mobility weighted 'transit stations',\n'retail and recreation', and 'grocery and pharmacy' visits",
+                                    y = "Other contacts") +
+  theme(legend.position = "none")
+plo
