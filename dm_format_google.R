63e88ed94ee204c4576c21ad6090ab2e69c391d3
Em Prestige <emiel200@gmail.com>
2022-11-01 10:21:46 +0000

    add line to save separate file for regional data

--- dm_format_google.R
+++ dm_format_google.R
@@ -8,6 +8,10 @@ mob_20 <- fread(file.path(data_path, "2020_GB_Region_Mobility_Report.csv"))
 mob_21 <- fread(file.path(data_path, "2021_GB_Region_Mobility_Report.csv"))
 mob_22 <- fread(file.path(data_path, "2022_GB_Region_Mobility_Report.csv"))
 
+region <- rbind(mob_20, mob_21, mob_22)
+
+qs::qsave(region, "C:\\Users\\emiel\\Documents\\LSHTM\\Fellowship\\Project\\comix_mobility\\Data\\google_mob_region.qs")
+
 UK_20 <- mob_20[sub_region_1 == ""]
 UK_21 <- mob_21[sub_region_1 == ""]
 UK_22 <- mob_22[sub_region_1 == ""]
