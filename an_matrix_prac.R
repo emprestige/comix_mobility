e5be03edbfe3e11ea9a70277165a687c33005059
Em Prestige <emiel200@gmail.com>
2022-11-04 11:54:59 +0000

    fix ordering of axis

--- an_matrix_prac.R
+++ an_matrix_prac.R
@@ -37,18 +37,25 @@ dt <- list()
 for (i in 1:101) {
   dt[[i]] <- melt(data.table(rounds[[i]]$matrix, keep.rownames = TRUE) ,
                   id.vars = c("rn"))
+  dt[[i]]$rn <- factor(dt[[i]]$rn, levels = c("[0,5)", "[5,12)", "[12,18)",
+                                              "[18,30)", "[30,40)", "[40,50)",
+                                              "[50,60)", "[60,65)", "[65,70)",
+                                              "70+"))
 }
+
+
 
 #create plots 
 plots <- list()
 for (i in 1:101) {
   plots[[i]] <- ggplot(data = dt[[i]], aes(x = rn, y = variable, fill = value)) + 
-    geom_tile()
+    geom_tile() + coord_equal()
 }
 
 #plot some to check 
 plots[[1]]
 plots[[28]]
-plots[[55]]
 plots[[101]]
 plots[[21]]
+
+dt[[21]][["rn"]]
