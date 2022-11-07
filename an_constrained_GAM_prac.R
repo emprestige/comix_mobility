20cb6c5417d24d4f81704a544ff5b2a554f8372e
Em Prestige <emiel200@gmail.com>
2022-11-07 14:02:52 +0000

    add restriction based color-coding

--- an_constrained_GAM_prac.R
+++ an_constrained_GAM_prac.R
@@ -10,11 +10,44 @@ library(lubridate)
 #set data path
 data_path <-"C:\\Users\\emiel\\Documents\\LSHTM\\Fellowship\\Project\\comix_mobility\\Data\\"
 
-#import mobility data
-mob <- qs::qread(file.path(data_path, "google_mob.qs"))
+#create sequence of dates
+date <- seq(as.Date("2020-03-02"), as.Date("2022-03-02"), by = "days")
+lockdowns <- as.data.table(as.Date(date))
+lockdowns$lockdown_status <- 0
+colnames(lockdowns) <- c("date", "status")
 
-#subset for same date range
+#create time intervals for different types of restrictions
+T1 <- interval(ymd("2020-03-02"), ymd("2020-03-22"))
+L1 <- interval(ymd("2020-03-23"), ymd("2020-05-31"))
+T2 <- interval(ymd("2020-06-01"), ymd("2020-07-04"))
+F1 <- interval(ymd("2020-07-05"), ymd("2020-09-13"))
+T3 <- interval(ymd("2020-09-14"), ymd("2020-11-04"))
+L2 <- interval(ymd("2020-11-05"), ymd("2020-12-01"))
+T4 <- interval(ymd("2020-12-02"), ymd("2021-01-05"))
+L3 <- interval(ymd("2021-01-06"), ymd("2021-03-07"))
+T5 <- interval(ymd("2021-03-08"), ymd("2021-07-18"))
+F2 <- interval(ymd("2021-07-19"), ymd("2022-03-02"))
+
+#assign value to each type of restriction
+lockdowns$status <- ifelse(ymd(lockdowns$date) %within% T1, 1, 
+                           ifelse(ymd(lockdowns$date) %within% L1, 2, 
+                           ifelse(ymd(lockdowns$date) %within% T2, 1, 
+                           ifelse(ymd(lockdowns$date) %within% T3, 1, 
+                           ifelse(ymd(lockdowns$date) %within% L2, 2, 
+                           ifelse(ymd(lockdowns$date) %within% T4, 1, 
+                           ifelse(ymd(lockdowns$date) %within% L3, 2, 
+                           ifelse(ymd(lockdowns$date) %within% T5, 1, 0))))))))
+
+#create factor
+lockdown_fac <- factor(lockdowns$status, levels = c(0, 1, 2, 3),
+                       labels = c("No restrictions", "Some restrictions",
+                                  "Lockdown", "Pre-Pandemic"))
+lockdowns$status <- lockdown_fac
+
+#merge mobility subset and lockdown information
+mob <- qs::qread(file.path(data_path, "google_mob.qs"))
 mob_sub <- mob[date >= "2020-03-23" & date <= "2022-03-02"]
+mob_sub_l <- merge(mob_sub, lockdowns, by = "date", all.y = F)
 
 #import contact data
 cnts <- qs::qread(file.path(data_path,"part_cnts.qs"))
@@ -72,24 +105,23 @@ another <- num[, .(work = mean(work), workplaces = mean(workplaces)),
                                    rep(0, length(date))), study)]
 
 #model using GAM
-model <- gam(work ~ s(workplaces), family = gaussian, data = another); lines(another$workplaces, fitted(model))
+model <- gam(work ~ s(workplaces), family = gaussian, data = another)
 
 #monotonic constraint
-x <- another$workplaces;
+x <- another$workplaces
 y <- another$work 
-dat <- data.frame(x=x,y=y)
+dat <- data.frame(x = x, y = y)
 # Show regular spline fit (and save fitted object)
-f.ug <- gam(y~s(x,k=10,bs="cr")); lines(x,fitted(f.ug))
+f.ug <- gam(y ~ s(x, k = 10, bs = "cr"))
 # Create Design matrix, constraints etc. for monotonic spline....
-sm <- smoothCon(s(x,k=10,bs="cr"),dat,knots=NULL)[[1]]
+sm <- smoothCon(s(x, k = 10, bs = "cr"), dat, knots = NULL)[[1]]
 F <- mono.con(sm$xp);   # get constraints
-G <- list(X=sm$X,C=matrix(0,0,0),sp=f.ug$sp,p=sm$xp,y=y,w=y*0+1)
+G <- list(X = sm$X, C = matrix(0, 0, 0), sp = f.ug$sp, p = sm$xp, y = y, w = y*0+1)
 G$Ain <- F$A;G$bin <- F$b;G$S <- sm$S;G$off <- 0
 
 p <- pcls(G);  # fit spline (using s.p. from unconstrained fit)
 
-fv<-Predict.matrix(sm,data.frame(x=x))%*%p
-lines(x,fv,col=2)
+fv <- Predict.matrix(sm, data.frame(x = x))%*%p
 dat <- as.data.table(x)
 dat$fv <- fv
 
@@ -122,21 +154,20 @@ another[, predictor := retail * 0.345 + transit * 0.445 + grocery * 0.210] # See
 model <- gam(other ~ s(predictor), family = gaussian, data = another)
 
 #monotonic constraint
-x <- another$predictor;
+x <- another$predictor
 y <- another$other
-dat <- data.frame(x=x,y=y)
+dat <- data.frame(x = x, y = y)
 # Show regular spline fit (and save fitted object)
-f.ug <- gam(y~s(x,k=10,bs="cr")); lines(x,fitted(f.ug))
+f.ug <- gam(y ~ s(x, k = 10, bs = "cr"))
 # Create Design matrix, constraints etc. for monotonic spline....
-sm <- smoothCon(s(x,k=10,bs="cr"),dat,knots=NULL)[[1]]
+sm <- smoothCon(s(x, k = 10, bs = "cr"), dat, knots = NULL)[[1]]
 F <- mono.con(sm$xp);   # get constraints
-G <- list(X=sm$X,C=matrix(0,0,0),sp=f.ug$sp,p=sm$xp,y=y,w=y*0+1)
+G <- list(X = sm$X, C = matrix(0, 0, 0), sp = f.ug$sp, p = sm$xp, y = y, w = y*0+1)
 G$Ain <- F$A;G$bin <- F$b;G$S <- sm$S;G$off <- 0
 
 p <- pcls(G);  # fit spline (using s.p. from unconstrained fit)
 
-fv<-Predict.matrix(sm,data.frame(x=x))%*%p
-lines(x,fv,col=2)
+fv <- Predict.matrix(sm, data.frame(x = x))%*%p
 dat <- as.data.table(x)
 dat$fv <- fv
 
@@ -161,21 +192,20 @@ another <- num[, .(home = mean(home), residential = mean(residential)),
                                     rep(0, length(date))), study)]
 
 #monotonic constraint
-x <- another$residential;
+x <- another$residential
 y <- another$home 
-dat <- data.frame(x=x,y=y)
+dat <- data.frame(x = x,y = y)
 # Show regular spline fit (and save fitted object)
-f.ug <- gam(y~s(x,k=10,bs="cr")); lines(x,fitted(f.ug))
+f.ug <- gam(y ~ s(x, k = 10,bs = "cr"))
 # Create Design matrix, constraints etc. for monotonic spline....
-sm <- smoothCon(s(x,k=10,bs="cr"),dat,knots=NULL)[[1]]
+sm <- smoothCon(s(x, k = 10, bs  ="cr"), dat, knots = NULL)[[1]]
 F <- mono.con(sm$xp);   # get constraints
-G <- list(X=sm$X,C=matrix(0,0,0),sp=f.ug$sp,p=sm$xp,y=y,w=y*0+1)
+G <- list(X = sm$X, C = matrix(0, 0, 0), sp = f.ug$sp, p = sm$xp, y = y, w = y*0+1)
 G$Ain <- F$A;G$bin <- F$b;G$S <- sm$S;G$off <- 0
 
 p <- pcls(G);  # fit spline (using s.p. from unconstrained fit)
 
-fv<-Predict.matrix(sm,data.frame(x=x))%*%p
-lines(x,fv,col=2)
+fv <- Predict.matrix(sm,data.frame(x = x))%*%p
 dat <- as.data.table(x)
 dat$fv <- fv
 
@@ -186,3 +216,122 @@ plh <- ggplot(another) +
        colour = "Study") +
   theme(legend.position = "none")
 plh
+
+##restriction based colour-coding
+#see top of script for lockdown dates 
+
+#create data table with subset of variables
+num <- cnts_date[, .(date, part_id, part_age, home = n_cnt_home, 
+                     work = n_cnt_work, school = n_cnt_school, other = n_cnt_other)]
+num[, t := as.numeric(date - ymd("2020-01-01"))]
+
+#duplicate google mobility data and rename columns
+gm2 <- rlang::duplicate(mob_sub_l)
+names(gm2) <- str_replace(names(gm2), "_percent_change_from_baseline", "")
+names(gm2) <- str_replace(names(gm2), "_and", "")
+
+#merge data tables 
+num <- merge(num, gm2[, c(1, 10:16)], by = "date", all.x = T)
+
+#create study column
+num[, study := "CoMix"]
+
+#import edited polymod data
+pnum <- qs::qread(file.path(data_path, "polymod.qs"))
+
+#create study column
+pnum[, study := "POLYMOD"]
+
+#add information for lockdown status (i.e. none)
+pnum[, status := "Pre-Pandemic"]
+
+#bind the rows together
+num <- rbind(num, pnum, fill = TRUE)
+
+#turn mobility data into decimals instead of percentages
+num[, retail_recreation := (100 + retail_recreation) * 0.01]
+num[, grocery_pharmacy  := (100 + grocery_pharmacy ) * 0.01]
+num[, parks             := (100 + parks            ) * 0.01]
+num[, transit_stations  := (100 + transit_stations ) * 0.01]
+num[, workplaces        := (100 + workplaces       ) * 0.01]
+num[, residential       := (100 + residential      ) * 0.01]
+
+#un-oversample young people from POLYMOD
+num <- rbind(
+  num[study == "CoMix"],
+  num[study == "POLYMOD" & part_age <= 20][seq(0, .N, by = 2)],
+  num[study == "POLYMOD" & part_age > 20]
+)
+
+#get fortnightly work data
+another <- num[, .(status = status, work = mean(work), workplaces = mean(workplaces)),
+               by = .(week = ifelse(study == "CoMix", paste(year(date), "/", 
+                                                      ceiling(week(date)/2)), 
+                                    rep(0, length(date))), study)]
+another <- unique(another)
+
+#monotonic constraint
+x <- another$workplaces
+y <- another$work 
+dat <- data.frame(x = x, y = y)
+# Show regular spline fit (and save fitted object)
+f.ug <- gam(y ~ s(x, k = 10, bs = "cr"))
+# Create Design matrix, constraints etc. for monotonic spline....
+sm <- smoothCon(s(x, k = 10, bs = "cr"), dat, knots = NULL)[[1]]
+F <- mono.con(sm$xp);   # get constraints
+G <- list(X = sm$X, C = matrix(0, 0, 0), sp = f.ug$sp, p = sm$xp, y = y, w = y*0+1)
+G$Ain <- F$A;G$bin <- F$b;G$S <- sm$S;G$off <- 0
+
+p <- pcls(G);  # fit spline (using s.p. from unconstrained fit)
+
+fv <- Predict.matrix(sm, data.frame(x = x))%*%p
+dat <- as.data.table(x)
+dat$fv <- fv
+
+#plot
+plw <- ggplot(another) + 
+  geom_point(aes(x = workplaces, y = work, colour = status)) + 
+  geom_line(data = dat, aes(x, fv)) + ylim(0, 2.5) +
+  labs(x = "Google Mobility\n'workplaces' visits", y = "Work contacts", 
+       colour = "Status") 
+plw
+
+#get fortnightly 'other' data
+another <- num[, .(status = status, other = mean(other), retail = mean(retail_recreation), 
+                   grocery = mean(grocery_pharmacy), transit = mean(transit_stations)), 
+               by = .(week = ifelse(study == "CoMix", paste(year(date), "/", 
+                                                      ceiling(week(date)/2)), 
+                                    rep(0, length(date))), study)]
+another <- unique(another)
+
+#create predictor from weighting of variables
+another[, predictor := retail * 0.345 + transit * 0.445 + grocery * 0.210] # See "optimisation" below for how this was arrived at
+
+#model using GAM
+model <- gam(other ~ s(predictor), family = gaussian, data = another)
+
+#monotonic constraint
+x <- another$predictor
+y <- another$other
+dat <- data.frame(x = x, y = y)
+# Show regular spline fit (and save fitted object)
+f.ug <- gam(y ~ s(x, k = 10, bs = "cr"))
+# Create Design matrix, constraints etc. for monotonic spline....
+sm <- smoothCon(s(x, k = 10, bs = "cr"), dat, knots = NULL)[[1]]
+F <- mono.con(sm$xp);   # get constraints
+G <- list(X = sm$X, C = matrix(0, 0, 0), sp = f.ug$sp, p = sm$xp, y = y, w = y*0+1)
+G$Ain <- F$A;G$bin <- F$b;G$S <- sm$S;G$off <- 0
+
+p <- pcls(G);  # fit spline (using s.p. from unconstrained fit)
+
+fv <- Predict.matrix(sm, data.frame(x = x))%*%p
+dat <- as.data.table(x)
+dat$fv <- fv
+
+#plot
+plo <- ggplot(another) + 
+  geom_point(aes(x = predictor, y = other, colour = status)) + 
+  geom_line(data = dat, aes(x, fv)) + ylim(0, 4) +
+  labs(x = "Google Mobility weighted 'transit stations',\n'retail and recreation', and 'grocery and pharmacy' visits", 
+       y = "Other contacts", colour = "Status")
+plo
