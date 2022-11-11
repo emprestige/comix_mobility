5b4df3aed10e8841ea9adf66d9e12fa22b186bef
Em Prestige <emiel200@gmail.com>
2022-11-11 14:04:33 +0000

    remove survey round 6

--- dm_clean_comix.R
+++ dm_clean_comix.R
@@ -19,7 +19,7 @@ ct_uk <- ct[country == "uk"]
 #filter out people who aren't adults 
 #pt_uk <- pt_uk[part_age >= "18" & part_age <= "65"]
 
-#get lower limits ofr age groups 
+#get lower limits of age groups 
 ages <- ct_uk %>% group_by(cnt_age_group) %>% tally()
 
 #filter out unnecessary variables 
@@ -27,6 +27,8 @@ ct_nmes <- c("part_id", "cnt_age_est_min", "cnt_age_est_max",
              "cnt_gender", "cnt_home", "cnt_work", "cnt_school",
              "cnt_public_transport", "cnt_other", "survey_round")
 ct_uk <- ct_uk[, ..ct_nmes]
+ct_uk <- ct_uk[survey_round != "6"]
+pt_uk <- pt_uk[survey_round != "6"]
 
 #save data
 qs::qsave(pt_uk, "C:\\Users\\emiel\\Documents\\LSHTM\\Fellowship\\Project\\comix_mobility\\Data\\participants.qs")
