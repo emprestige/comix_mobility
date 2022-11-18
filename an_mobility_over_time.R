1d16db987e6ecc5f727ad76fd57ee147ba1d110b
Em Prestige <emiel200@gmail.com>
2022-11-18 11:29:40 +0000

    google mobility over time (months)

---
+++ an_mobility_over_time.R
@@ -0,0 +1,84 @@
+##mobility over time 
+
+#load libraries
+library(data.table)
+library(ggplot2)
+library(tidyverse)
+library(mgcv)
+library(lubridate)
+library(cowplot)
+library(zoo)
+library(matrixStats)
+
+#set cowplot theme
+theme_set(cowplot::theme_cowplot(font_size = 10) + theme(strip.background = element_blank()))
+
+#set data path
+data_path <-"C:\\Users\\emiel\\Documents\\LSHTM\\Fellowship\\Project\\comix_mobility\\Data\\"
+
+#import mobility data
+mob <- qs::qread(file.path(data_path, "google_mob.qs"))
+
+#subset for same date range
+mob_sub <- mob[date >= "2020-03-23" & date <= "2022-03-02"]
+
+#duplicate google mobility data and rename columns
+gm2 <- rlang::duplicate(mob_sub)
+names(gm2) <- str_replace(names(gm2), "_percent_change_from_baseline", "")
+names(gm2) <- str_replace(names(gm2), "_and", "")
+
+#turn mobility data into decimals instead of percentages
+gm2[, retail_recreation := (100 + retail_recreation) * 0.01]
+gm2[, grocery_pharmacy  := (100 + grocery_pharmacy ) * 0.01]
+gm2[, parks             := (100 + parks            ) * 0.01]
+gm2[, transit_stations  := (100 + transit_stations ) * 0.01]
+gm2[, workplaces        := (100 + workplaces       ) * 0.01]
+gm2[, residential       := (100 + residential      ) * 0.01]
+
+#get averages (weekly to start?)
+gm_av <- gm2[, .(retail = mean(retail_recreation), 
+                 grocery = mean(grocery_pharmacy),
+                 parks = mean(parks), 
+                 transit = mean(transit_stations),
+                 workplaces = mean(workplaces),
+                 residential = mean(residential)),
+             by = .(month = paste(year(date), "/", month(date)))]
+
+#plot retail
+retail <- ggplot(gm_av) + 
+  geom_line(aes(month, retail), group = 1) + 
+  guides(x = guide_axis(angle = 90))
+retail
+
+#plot grocery
+grocery <- ggplot(gm_av) +
+  geom_line(aes(month, grocery), group = 1) + 
+  guides(x = guide_axis(angle = 90))
+grocery
+
+#plot parks
+parks <- ggplot(gm_av) + 
+  geom_line(aes(month, parks), group = 1) + 
+  guides(x = guide_axis(angle = 90))
+parks
+
+#plot transit
+transit <- ggplot(gm_av) + 
+  geom_line(aes(month, transit), group = 1) + 
+  guides(x = guide_axis(angle = 90))
+transit
+
+#workplaces
+workplaces <- ggplot(gm_av) +
+  geom_line(aes(month, workplaces), group = 1) + 
+  guides(x = guide_axis(angle = 90))
+workplaces
+
+#plot residential
+residential <- ggplot(gm_av) +
+  geom_line(aes(month, residential), group = 1) + 
+  guides(x = guide_axis(angle = 90))
+residential
+
+#plot together
+plot_grid(retail, grocery, parks, transit, workplaces, residential)
