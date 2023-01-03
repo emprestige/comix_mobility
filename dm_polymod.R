58ace87986cd1e2f9cee175ab076f21b055ee8a6
Em Prestige <emiel200@gmail.com>
2023-01-03 11:29:56 +0000

    fix

--- dm_polymod.R
+++ dm_polymod.R
@@ -21,11 +21,11 @@ poly[, d_school    := ifelse(part_age < 18, cnt_school == 1 & cnt_home == 0,
 poly[, d_work      := ifelse(part_age >= 18, cnt_work == 1 & cnt_home == 0,
                              cnt_work == 1 & cnt_home == 0 & cnt_school == 0)]
 poly[, d_other     := (cnt_transport + cnt_leisure + cnt_otherplace) >= 1 & cnt_home == 0 & cnt_school == 0 & cnt_work == 0]
-poly = poly[!is.na(d_home)] # only cuts out 6 contacts.
+poly[, d_phys      := phys_contact == 1]
 
 #sum contacts
 pnum = poly[, .(home = sum(d_home), work = sum(d_work), school = sum(d_school), 
-                other = sum(d_other), phys = sum(phys_contact)), 
+                other = sum(d_other), phys = sum(d_phys)), 
             by = .(part_id, part_age, date = ymd(sday_id))]
 pnum = pnum[, lapply(.SD, function(x) pmin(50, x)), by = .(part_id, part_age, date), 
             .SDcols = c("home", "work", "school", "other", "phys")]
@@ -33,12 +33,6 @@ pnum = pnum[, lapply(.SD, function(x) pmin(50, x)), by = .(part_id, part_age, da
 #insert extra variables 
 pnum[, t := as.numeric(date - ymd("2006-01-01"))]
 pnum[, all := home + work + school + other]
-# pnum[, retail_recreation := 0]
-# pnum[, grocery_pharmacy := 0]
-# pnum[, parks := 0]
-# pnum[, transit_stations := 0]
-# pnum[, workplaces := 0]
-# pnum[, residential := 0]
 pnum[, study := "POLYMOD"]
 
 #save
